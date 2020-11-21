d5e1465229d0b4d1bef02f7a1306865b4930d567
Alva Chien <alvachien@live.com>
2020-11-22 00:19:43 +0800

    Add new codes

--- java-tutorial/basic-knowlege/src/main/java/com/alvachien/learning/App.java
+++ java-tutorial/basic-knowlege/src/main/java/com/alvachien/learning/App.java
@@ -1,5 +1,6 @@
 package com.alvachien.learning;
 
+import java.util.Scanner;
 /**
  * Hello world!
  *
@@ -9,5 +10,41 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+
+        // String
+        String s = "ABC\n\u4e2d\u6587"; // 包含6个字符: A, B, C, 换行符, 中, 文
+        System.out.println(s);
+
+        String t = """
+                   SELECT * FROM
+                     users
+                   WHERE id > 100
+                   ORDER BY name DESC
+                   """;
+        System.out.println(t);
+
+        // Array
+        int[] ns = new int[5];
+        System.out.println(ns.length); // 5
+
+        int[] ns2;
+        ns2 = new int[] { 68, 79, 91, 85, 62 };
+        System.out.println(ns2.length); // 5
+        ns = new int[] { 1, 2, 3 };
+        System.out.println(ns.length); // 3
+
+        // Printf
+        double d = 3.1415926;
+        System.out.printf("%.2f\n", d); // 显示两位小数3.14
+        System.out.printf("%.4f\n", d); // 显示4位小数3.1416
+
+        // Scanner
+        Scanner scanner = new Scanner(System.in); // 创建Scanner对象
+        System.out.print("Input your name: "); // 打印提示
+        String name = scanner.nextLine(); // 读取一行输入并获取字符串
+        System.out.print("Input your age: "); // 打印提示
+        int age = scanner.nextInt(); // 读取一行输入并获取整数
+        System.out.printf("Hi, %s, you are %d\n", name, age); // 格式化输出
+        scanner.close();        
     }
 }
