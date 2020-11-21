a04610ccffc4bb398ea1aa4d73f2b7af0ed2d25f
Alva Chien <alvachien@live.com>
2020-11-21 23:34:49 +0800

    Add new projects

---
+++ maven-tutorial-2/README.md
@@ -0,0 +1,4 @@
+# Mavin-Tutorial
+
+[链接](https://alvachien.github.io/2020/11/maven-002.html)
+
---
+++ maven-tutorial-2/maven-way-1/module-a/src/main/java/com/alvachien/app/App.java
@@ -0,0 +1,13 @@
+package com.alvachien.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
