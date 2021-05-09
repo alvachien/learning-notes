fbbca46dcf582fc02c183ba681f2cf860c1a50b1
Alva Chien <alvachien@live.com>
2021-05-09 20:49:36 +0800

    Add new codes

---
+++ crazy-java-16-lessons/01-array-memory/ArrayTest.java
@@ -0,0 +1,14 @@
+public class ArrayTest
+{
+    public static void main(String[] args) 
+    {
+        String[] books = new String[]
+        {
+            "Book 1",
+            "Book 2",
+            "Book 3",
+            "Book 4"
+        };
+
+    }
+}
