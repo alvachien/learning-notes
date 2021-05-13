588e9fbdc29a0f52346688afa8b86250c0034321
Alva Chien <alvachien@live.com>
2021-05-13 23:38:42 +0800

    Learning new notes

--- crazy-java-16-lessons/01-array-memory/ArrayTest.java
+++ crazy-java-16-lessons/01-array-memory/ArrayTest.java
@@ -1,3 +1,5 @@
+import javax.sound.midi.SysexMessage;
+
 public class ArrayTest
 {
     public static void main(String[] args) 
@@ -10,5 +12,40 @@ public class ArrayTest
             "Book 4"
         };
 
+        String[] names = {
+            "name1",
+            "name2",
+            "name3"
+        };
+        String[] arArr = new String[5];
+
+        String strCreateBooks = """
+        String[] books = new String[] 
+        {
+            "Book 1",
+            "Book 2",
+            "Book 3",
+            "Book 4"
+        };
+        """;
+        System.out.print(strCreateBooks);
+        System.out.println("Length of books: " + books.length);
+        System.out.println();
+
+        String strCreateNames = """
+        String[] names = {
+            "name1",
+            "name2",
+            "name3"
+        };
+        """;
+        System.out.print(strCreateNames);
+        System.out.println("Length of names: " + names.length);
+        System.out.println();
+
+        String strCreateArray = "String[] arArr = new String[5];";
+        System.out.print(strCreateArray);
+        System.out.println("Length of arArr: " + arArr.length);
+        System.out.println();
     }
 }
---
+++ crazy-java-16-lessons/01-array-memory/TwoDimensionArray.java
@@ -0,0 +1,9 @@
+public class TwoDimensionArray {
+    public static void main(String[] args) {
+        int[][] a;
+        a = new int[4][];
+        for (int i = 0; i < a.length; i ++ ) {
+            System.out.println(a[i]);
+        }
+    }
+}
