e9f5de6024bfeb9d1c62b7b1c918bb2fc9c2b011
Alva Chien <alvachien@live.com>
2022-05-21 23:58:23 +0800

    Add new codes

--- python-tutorial/python-exercises/week6_lib_final.py
+++ python-tutorial/python-exercises/week6_lib_final.py
@@ -1,2 +1,14 @@
-import math
+import requests
 
+searchterm = input("Please enter a search term:")
+
+payload = {'term': searchterm, 'entity': 'album'}
+resp = requests.get('https://itunes.apple.com/search', params=payload)
+# print(resp.url)
+
+respbody = resp.json()
+
+# print(respbody)
+print(f"The search returned {respbody['resultCount']} results")
+for result in respbody["results"]:
+    print(f"Artist: {result['artistName']} - Album: {result['artistName']} - Track Count: {result['trackCount']}")
