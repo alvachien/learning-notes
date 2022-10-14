70695701b63c5d65f66e57173f7c78cd73330f03
Alva Chien <alvachien@163.com>
2022-10-14 22:54:06 +0800

    Thymeleaf demo

---
+++ spring-tutorial/thymeleaf-jpa-demo/src/main/java/com/alvachien/springtutorial/thymeleafjpademo/ThymeleafJpaDemoApplication.java
@@ -0,0 +1,13 @@
+package com.alvachien.springtutorial.thymeleafjpademo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafJpaDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymeleafJpaDemoApplication.class, args);
+	}
+
+}
---
+++ spring-tutorial/thymeleaf-jpa-demo/src/test/java/com/alvachien/springtutorial/thymeleafjpademo/ThymeleafJpaDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alvachien.springtutorial.thymeleafjpademo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafJpaDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
